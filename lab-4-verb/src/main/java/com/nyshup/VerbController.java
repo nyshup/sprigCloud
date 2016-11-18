b4fc8b2174453ab9686a37c7403ec3528be1bf4f
Ruslan Pushyn <nyshup@gmail.com>
2016-11-18 15:47:15 +0200

    update

--- lab-4-verb/src/main/java/com/nyshup/VerbController.java
+++ lab-4-verb/src/main/java/com/nyshup/VerbController.java
@@ -3,10 +3,12 @@ package com.nyshup;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by ruslan on 11/18/16.
  */
+@RestController
 public class VerbController {
 
     @Value("${words}")
